09ce3a143372ab1863b3625dc363ed89e04d5247
Yana Derevianko <y.derevianko@viseven.com>
2021-09-26 20:33:01 +0300

    hooks

---
+++ src/components/App/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import { ContactsForm } from "../ContactsForm/ContactsForm";
+import { ContactList } from "../ContactList/ContactList";
+import { Filter } from "../Filter/Filter";
+import { Container } from "./App.styled";
+import shortid from "shortid";
+
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      setContacts({ contacts: parsedContacts });
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts])
+
+  const addContact = ({ name, number }) => {
+
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    setContacts(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    if (contacts.length) {
+      return contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(normalizedFilter)
+      );
+    }
+  };
+  const changeFilter = (e) => {
+    const { name, value } = e.target;
+    setFilter({
+      [name]: value,
+    });
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <Container>
+      <ContactsForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      {contacts.length > 1 && <Filter onChange={changeFilter} />}
+      {contacts.length ? (
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <p>There are no contacts here</p>
+      )}
+    </Container>
+  );
+}
